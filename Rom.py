7b483780dba6d0be448d1500791774df7f027549
codemann8 <codemann8@gmail.com>
2026-01-11 00:07:20 -0600

    Fixed bonk drops duplicate counting and potentially writing bad values

--- Rom.py
+++ Rom.py
@@ -43,7 +43,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = 'f2eebfbec9c8ad638e922ed1047d1c10'
+RANDOMIZERBASEHASH = 'b8eedc36b1c63dc295367f16868ddf89'
 
 
 class JsonRom(object):
