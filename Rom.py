00ba5d89fb2d3b1da4aea5836348757ce1cb58fa
codemann8 <codemann8@gmail.com>
2026-01-19 15:21:13 -0600

    Fixes issue not able to screen transition if bumped by enemy in water

--- Rom.py
+++ Rom.py
@@ -43,7 +43,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '35a7c1a26275a7bde5de05b39690700c'
+RANDOMIZERBASEHASH = '01d81a1ad16b1838b27a057b8f8936d4'
 
 
 class JsonRom(object):
