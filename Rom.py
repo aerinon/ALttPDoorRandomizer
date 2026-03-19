d58518fbac4d04d63d02f00b657c4dbb165e1a65
codemann8 <codemann8@gmail.com>
2026-03-19 17:48:54 -0500

    2604 Baserom

--- Rom.py
+++ Rom.py
@@ -48,7 +48,7 @@ JAP10HASH = '03a63945398191337e896e5771f77173'
 RANDOMIZERBASEHASH = 'f1d838125b574639a74cf3de462da7d0'
 
 limited_run_hashes = {
-    '2604' : 'f4aee210d706a0af2dd638868e88f41c',
+    '2604' : '8be64ab023a82052a1f7f2e74a052f2f',
 }
 
 class JsonRom(object):
