d7894fb11ea3e09346420da0c39b80e170a0fe42
aerinon <randall.rupper@gmail.com>
2026-04-03 07:48:06 -0600

    baserom: update

--- Rom.py
+++ Rom.py
@@ -48,7 +48,7 @@ JAP10HASH = '03a63945398191337e896e5771f77173'
 RANDOMIZERBASEHASH = '17e19fe2d62915e58dcf31e71524ce29'
 
 limited_run_hashes = {
-    '2604' : '4103b0f4ca9cafab4c7982ef4fa4f1f6',
+    '2604' : '5a35c2a52c983ecc5150c96d6c0dfdc4',
 }
 
 class JsonRom(object):
