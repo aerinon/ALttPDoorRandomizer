3c8fb314929ae8bd9cae7e641719731a5b32b1a7
codemann8 <codemann8@gmail.com>
2026-04-03 23:37:05 -0500

    2604 fix false pyramid ped

--- Rom.py
+++ Rom.py
@@ -48,7 +48,7 @@ JAP10HASH = '03a63945398191337e896e5771f77173'
 RANDOMIZERBASEHASH = '17e19fe2d62915e58dcf31e71524ce29'
 
 limited_run_hashes = {
-    '2604' : '7586b836c650aca20c61a362857dcaa7',
+    '2604' : 'd879e91af4cbeea7d6fc49281d0300ae',
 }
 
 class JsonRom(object):
